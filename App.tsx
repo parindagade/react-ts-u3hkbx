5d5b4cbd88ede3be0ef65ac34a3b74ac626b79d1
parindagade <124175099+parindagade@users.noreply.github.com>
2023-03-17 17:28:57 +0530

    2 files modified

--- App.tsx
+++ App.tsx
@@ -4,9 +4,7 @@ import './style.css';
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-      <h2>Hello Everyone I am learning React</h2>
+      <p>Hello from React</p>
     </div>
   );
 }
